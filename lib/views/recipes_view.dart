90c783c86425ccc30784046525aef456ffcec103
HazimNabil <HazemNabil95@outlook.com>
2024-06-29 23:46:42 +0200

    Add navigation between pages

--- lib/views/recipes_view.dart
+++ lib/views/recipes_view.dart
@@ -3,17 +3,34 @@ import 'package:flutter/material.dart';
 import '../constans.dart';
 import '../helper/create_app_bar.dart';
 import '../widgets/home_page.dart';
+import '../widgets/random_recipe_page.dart';
+import '../widgets/saved_recipes_page.dart';
 
-class RecipesView extends StatelessWidget {
+class RecipesView extends StatefulWidget {
   const RecipesView({super.key});
 
+  @override
+  State<RecipesView> createState() => _RecipesViewState();
+}
+
+class _RecipesViewState extends State<RecipesView> {
+  int _currentIndex = 0;
+  final _pages = const [
+    HomePage(),
+    SavedRecipesPage(),
+    RandomRecipePage(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: createAppBar("Let's Cooking"),
-      body: const HomePage(),
+      body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
         selectedItemColor: kMainColor,
+        onTap: (newIndex) {
+          setState(() => _currentIndex = newIndex);
+        },
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home_outlined),
---
+++ lib/widgets/random_recipe_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RandomRecipePage extends StatelessWidget {
+  const RandomRecipePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

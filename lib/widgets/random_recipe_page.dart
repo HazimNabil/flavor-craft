7ba331ececade56e600d770434eb7c4d5c5e7d3a
HazimNabil <HazemNabil95@outlook.com>
2024-06-30 00:27:55 +0200

    Make random recipe page

--- lib/widgets/random_recipe_page.dart
+++ lib/widgets/random_recipe_page.dart
@@ -1,3 +1,5 @@
+import 'package:flavor_craft/constans.dart';
+import 'package:flavor_craft/widgets/recipe_card.dart';
 import 'package:flutter/material.dart';
 
 class RandomRecipePage extends StatelessWidget {
@@ -5,6 +7,28 @@ class RandomRecipePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          const RecipeCard(),
+          ElevatedButton.icon(
+            style: const ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll(kMainColor),
+              iconColor: MaterialStatePropertyAll(Colors.white),
+              fixedSize: MaterialStatePropertyAll(Size(180, 60)),
+              iconSize: MaterialStatePropertyAll(30),
+            ),
+            onPressed: () {},
+            icon: const Icon(Icons.restart_alt),
+            label: const Text(
+              'Regenerate',
+              style: TextStyle(color: Colors.white, fontSize: 21),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

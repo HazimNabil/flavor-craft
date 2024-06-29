04f049303043bf5d238a04571f3d7f2078ea55c3
HazimNabil <HazemNabil95@outlook.com>
2024-06-29 22:12:58 +0200

    Create navigation bar

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../constans.dart';
 import '../helper/create_app_bar.dart';
 import '../widgets/home_view_body.dart';
 
@@ -11,6 +12,23 @@ class HomeView extends StatelessWidget {
     return Scaffold(
       appBar: createAppBar("Let's Cooking"),
       body: const HomeViewBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: kMainColor,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark_border),
+            label: 'Saved',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shuffle),
+            label: 'Random',
+          ),
+        ],
+      ),
     );
   }
 }

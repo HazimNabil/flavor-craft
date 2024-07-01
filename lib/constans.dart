e598fa609326c12ccdf97f9b731052dd47d8a661
HazimNabil <HazemNabil95@outlook.com>
2024-07-01 22:29:42 +0200

    Handle fetchRecipes method exceptions

--- lib/constans.dart
+++ lib/constans.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const kMainColor = Color(0xFFD99651);
+const defaultErrorMessage = 'Oops there was an error, try later.';
--- lib/services/search_recipe_service.dart
+++ lib/services/search_recipe_service.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:flavor_craft/models/recipe_model.dart';
 
+import '../constans.dart';
+
 class SearchRecipeService {
   var apiKey = '1e3fa2dd0e654c96a374fe631d0aa652';
   var baseUrl = 'https://api.spoonacular.com/recipes';
@@ -14,9 +16,16 @@ class SearchRecipeService {
   }) async {
     String url =
         '$baseUrl/complexSearch?apiKey=$apiKey&query=$query&addRecipeInformation=true&fillIngredients=true&type=$category';
-    var response = await Dio().get(url);
-    List<dynamic> jsonData = response.data['results'];
-    var recipes = jsonData.map((recipe) => Recipe.fromJson(recipe)).toList();
-    return recipes;
+    try {
+      var response = await Dio().get(url);
+      List<dynamic> jsonData = response.data['results'];
+      var recipes = jsonData.map((recipe) => Recipe.fromJson(recipe)).toList();
+      return recipes;
+    } on DioException catch (e) {
+      var errorMessage = e.message ?? defaultErrorMessage;
+      throw Exception(errorMessage);
+    } catch (e) {
+      throw Exception(defaultErrorMessage);
+    }
   }
 }

515b431a37249ea2de53414c569c9fd3cef255f5
HazimNabil <HazemNabil95@outlook.com>
2024-07-02 21:53:04 +0200

    Display data in random recipe page

---
+++ lib/widgets/recipe_card_builder.dart
@@ -0,0 +1,35 @@
+import 'package:dio/dio.dart';
+import 'package:flavor_craft/models/recipe_model.dart';
+import 'package:flavor_craft/services/recipe_service.dart';
+import 'package:flavor_craft/widgets/recipe_card.dart';
+import 'package:flutter/material.dart';
+
+class RecipeCardBuilder extends StatefulWidget {
+  const RecipeCardBuilder({super.key});
+
+  @override
+  State<RecipeCardBuilder> createState() => _RecipeCardBuilderState();
+}
+
+class _RecipeCardBuilderState extends State<RecipeCardBuilder> {
+  dynamic future;
+
+  @override
+  void initState() {
+    super.initState();
+    future = RecipeService(Dio()).fetchRandomRecipe();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Recipe>(
+      future: future,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return RecipeCard(recipe: snapshot.data!);
+        }
+        return const Placeholder();
+      },
+    );
+  }
+}

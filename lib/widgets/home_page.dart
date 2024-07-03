02c30db134031c08c897778ccdc0761643d92d39
HazimNabil <HazemNabil95@outlook.com>
2024-07-04 00:31:06 +0200

    Provide SearchRecipeCubit in home page

--- lib/widgets/home_page.dart
+++ lib/widgets/home_page.dart
@@ -1,7 +1,9 @@
+import 'package:flavor_craft/cubits/search_recipe_cubit/search_recipe_cubit.dart';
 import 'package:flavor_craft/widgets/recipe_field.dart';
 import 'package:flavor_craft/widgets/recipe_title.dart';
 import 'package:flavor_craft/widgets/recipes_list_builder.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'category_section.dart';
 
@@ -10,31 +12,34 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
-      child: CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(
-            child: SizedBox(height: 55),
-          ),
-          SliverToBoxAdapter(
-            child: RecipeTitle(title: "Let's Cooking"),
-          ),
-          SliverToBoxAdapter(
-            child: SizedBox(height: 15),
-          ),
-          RecipeField(
-            hint: 'Search recipe',
-          ),
-          SliverToBoxAdapter(
-            child: SizedBox(height: 15),
-          ),
-          CategorySection(),
-          SliverToBoxAdapter(
-            child: SizedBox(height: 15),
-          ),
-          RecipesListBuilder()
-        ],
+    return BlocProvider(
+      create: (context) => SearchRecipeCubit(),
+      child: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0),
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: SizedBox(height: 55),
+            ),
+            SliverToBoxAdapter(
+              child: RecipeTitle(title: "Let's Cooking"),
+            ),
+            SliverToBoxAdapter(
+              child: SizedBox(height: 15),
+            ),
+            RecipeField(
+              hint: 'Search recipe',
+            ),
+            SliverToBoxAdapter(
+              child: SizedBox(height: 15),
+            ),
+            CategorySection(),
+            SliverToBoxAdapter(
+              child: SizedBox(height: 15),
+            ),
+            RecipesListBuilder()
+          ],
+        ),
       ),
     );
   }

26478cbea2fe831a3de9c8af87dd33778c4c1de8
HazimNabil <HazemNabil95@outlook.com>
2024-07-04 00:25:44 +0200

    Create SearchRecipeCubit

---
+++ lib/cubits/search_recipe_cubit/search_recipe_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:flavor_craft/cubits/search_recipe_cubit/search_recipe_states.dart';
+import 'package:flavor_craft/services/recipe_service.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SearchRecipeCubit extends Cubit<SearchRecipeState> {
+  SearchRecipeCubit() : super(SearchRecipeInitial()) {
+    searchRecipe();
+  }
+
+  Future<void> searchRecipe({String query = ''}) async {
+    emit(SearchRecipeLoading());
+    try {
+      var recipes = await RecipeService(Dio()).fetchRecipes(query: query);
+      emit(SearchRecipeLoaded(recipes));
+    } catch (e) {
+      emit(SearchRecipeError(e.toString()));
+    }
+  }
+}

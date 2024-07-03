ec973cda187242dde54871947794f143113333bb
HazimNabil <HazemNabil95@outlook.com>
2024-07-03 21:09:54 +0200

    Create RandomRecipeCubit

---
+++ lib/cubits/random_recipe_cubit/random_recipe_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:flavor_craft/cubits/random_recipe_cubit/random_recipe_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class RandomRecipeCubit extends Cubit<RandomRecipeState> {
+  RandomRecipeCubit(super.initialState);
+}

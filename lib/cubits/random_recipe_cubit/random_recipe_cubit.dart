0a54f8a44504f13b04a8139138f00bdf7dbe581f
HazimNabil <HazemNabil95@outlook.com>
2024-07-03 21:44:30 +0200

    Create RandomRecipeCubit method

--- lib/cubits/random_recipe_cubit/random_recipe_cubit.dart
+++ lib/cubits/random_recipe_cubit/random_recipe_cubit.dart
@@ -1,6 +1,20 @@
+import 'package:dio/dio.dart';
 import 'package:flavor_craft/cubits/random_recipe_cubit/random_recipe_states.dart';
+import 'package:flavor_craft/services/recipe_service.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class RandomRecipeCubit extends Cubit<RandomRecipeState> {
-  RandomRecipeCubit(super.initialState);
+  RandomRecipeCubit() : super(RandomRecipeInitial()) {
+    fetchRandomRecipe();
+  }
+
+  Future<void> fetchRandomRecipe() async {
+    try {
+      emit(RandomRecipeLoading());
+      await RecipeService(Dio()).fetchRandomRecipe();
+      emit(RandomRecipeLoaded());
+    } catch (e) {
+      emit(RandomRecipeError(e.toString()));
+    }
+  }
 }

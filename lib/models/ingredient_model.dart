a9d56070bc7d78592dce323d73a9874b2cef8c2f
HazimNabil <HazemNabil95@outlook.com>
2024-07-06 23:53:14 +0200

    Create type adapters for the models

--- lib/models/ingredient_model.dart
+++ lib/models/ingredient_model.dart
@@ -1,6 +1,14 @@
-class Ingredient {
+import 'package:hive/hive.dart';
+
+part 'ingredient_model.g.dart';
+
+@HiveType(typeId: 0)
+class Ingredient extends HiveObject {
+  @HiveField(0)
   final String name;
+  @HiveField(1)
   final double amount;
+  @HiveField(2)
   final String unit;
 
   Ingredient({
--- lib/models/recipe_model.dart
+++ lib/models/recipe_model.dart
@@ -1,10 +1,19 @@
+import 'package:hive/hive.dart';
 import 'ingredient_model.dart';
 
-class Recipe {
+part 'recipe_model.g.dart';
+
+@HiveType(typeId: 1)
+class Recipe extends HiveObject {
+  @HiveField(0)
   final int id;
+  @HiveField(1)
   final String title;
+  @HiveField(2)
   final String source;
+  @HiveField(3)
   final String image;
+  @HiveField(4)
   final List<Ingredient> ingredients;
 
   Recipe({

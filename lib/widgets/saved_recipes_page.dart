849038e9ee58866dc12267363968f1fb7b95911f
HazimNabil <HazemNabil95@outlook.com>
2024-07-07 21:49:37 +0200

    Provide ReadRecipe cubit

--- lib/widgets/saved_recipes_page.dart
+++ lib/widgets/saved_recipes_page.dart
@@ -1,4 +1,6 @@
+import 'package:flavor_craft/cubits/read_recipes_cubit/read_recipes_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'recipe_field.dart';
 import 'recipe_title.dart';
@@ -9,31 +11,34 @@ class SavedRecipesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: CustomScrollView(
-        slivers: [
-          const SliverToBoxAdapter(
-            child: SizedBox(height: 55),
-          ),
-          const SliverToBoxAdapter(
-            child: RecipeTitle(title: 'Saved Recipe'),
-          ),
-          const SliverToBoxAdapter(
-            child: SizedBox(height: 15),
-          ),
-          RecipeField(
-            hint: 'Search saved recipe',
-            onSubmitted: (query) {},
-          ),
-          const SliverToBoxAdapter(
-            child: SizedBox(height: 15),
-          ),
-          const SliverToBoxAdapter(
-            child: SizedBox(height: 15),
-          ),
-          // RecipesList()
-        ],
+    return BlocProvider(
+      create: (context) => ReadRecipesCubit(),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: CustomScrollView(
+          slivers: [
+            const SliverToBoxAdapter(
+              child: SizedBox(height: 55),
+            ),
+            const SliverToBoxAdapter(
+              child: RecipeTitle(title: 'Saved Recipe'),
+            ),
+            const SliverToBoxAdapter(
+              child: SizedBox(height: 15),
+            ),
+            RecipeField(
+              hint: 'Search saved recipe',
+              onSubmitted: (query) {},
+            ),
+            const SliverToBoxAdapter(
+              child: SizedBox(height: 15),
+            ),
+            const SliverToBoxAdapter(
+              child: SizedBox(height: 15),
+            ),
+            // RecipesList()
+          ],
+        ),
       ),
     );
   }
